d8763b3a5359c17cbb6192cfc5d7e23be22adfd8
joannayhs <joannayuhas@gmail.com>
2022-08-16 15:53:56 -0400

    added more details to cocktail details component

--- src/components/CocktailDetails.js
+++ src/components/CocktailDetails.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-export default function CocktailDetails(){
+export default function CocktailDetails({cocktail}){
     return(
-        <>
-        
-        </>
+        <div>
+            <p>{cocktail.strAlcoholic}</p>
+            <img src={cocktail.strDrinkThumb}/>
+            <p>{cocktail.strGlass}</p>
+            <p>{cocktail.strIngredient1}{cocktail.strMeasure1}</p>
+            <p>{cocktail.strIngredient2}{cocktail.strMeasure2}</p>
+            <p>{cocktail.strIngredient3}{cocktail.strMeasure3}</p>
+            <p>{cocktail.strIngredient4}{cocktail.strMeasure4}</p>
+            <p>{cocktail.strIngredient5}{cocktail.strMeasure5}</p>
+            <p>{cocktail.strInstructions}</p>
+        </div>
     )
 }
