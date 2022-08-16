4bfd9d7abc7ce586f1addf0d86b47240855df4cb
joannayhs <joannayuhas@gmail.com>
2022-08-16 15:43:30 -0400

    created cocktail details component

--- src/components/CocktailCard.js
+++ src/components/CocktailCard.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import {Link}  from  'react-router-dom'
 
 function CocktailCard({cocktail}){
 
     return (
         <div className="cocktail-card">
             <img src={cocktail.strDrinkThumb} atl={cocktail.strDrink} width="75%" /><br/>
-            {cocktail.strDrink}<br/>
+            <Link to={`/cocktails/${cocktail.idDrink}`}>{cocktail.strDrink}</Link><br/>
             {cocktail.strAlcoholic}<br/>
             {cocktail.strCategory}<br/>
         </div>
