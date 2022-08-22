ef0a4746230de8df5910bece38ca2955c174f70d
joannayhs <joannayuhas@gmail.com>
2022-08-22 17:00:37 -0400

    added name attr and line breaks

--- src/components/NewRecipe.js
+++ src/components/NewRecipe.js
@@ -12,20 +12,20 @@ export default function NewRecipe(){
     return (
         <>
          <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Drink Name"/>
-            <input type="text" placeholder="Category"/>
-            <input type="text" placeholder="Instructions"/>
-            <input type="text" placeholder="image URL"/>
-            <input type="text" placeholder="Ingredient 1"/>
-            <input type="text" placeholder="Measurement 1"/>
-            <input type="text" placeholder="Ingredient 2"/>
-            <input type="text" placeholder="Measurement 2"/>
-            <input type="text" placeholder="Ingredient 3"/>
-            <input type="text" placeholder="Measurement 3"/>
-            <input type="text" placeholder="Ingredient 4"/>
-            <input type="text" placeholder="Measurement 4"/>
-            <input type="text" placeholder="Ingredient 5"/>
-            <input type="text" placeholder="Measurement 5"/>
+            <input type="text" placeholder="Drink Name" name="strDrink"/> <br/>
+            <input type="text" placeholder="Category" name="strCategory"/> <br/>
+            <input type="text" placeholder="Instructions" name="strInstructions"/> <br/>
+            <input type="text" placeholder="image URL" name="strDrinkThumb"/> <br/>
+            <input type="text" placeholder="Ingredient 1" name="strIngredient1"/> <br/>
+            <input type="text" placeholder="Measurement 1" name="strMeasure1"/> <br/>
+            <input type="text" placeholder="Ingredient 2" name="strIngredient2"/> <br/>
+            <input type="text" placeholder="Measurement 2" name="strMeasure2" /> <br/>
+            <input type="text" placeholder="Ingredient 3" name="strMeasure3" /> <br/>
+            <input type="text" placeholder="Measurement 3" name="strMeasure3" /> <br/>
+            <input type="text" placeholder="Ingredient 4" name="strMeasure4" /> <br/>
+            <input type="text" placeholder="Measurement 4" name="strMeasure4" /> <br/>
+            <input type="text" placeholder="Ingredient 5" name="strMeasure5" /> <br/>
+            <input type="text" placeholder="Measurement 5" name="strMeasure5" /> <br/>
             <button>Submit</button>
         </form>
         </>
