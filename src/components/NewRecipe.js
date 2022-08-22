e32b389eda8645f73603b89901671c78c06ba51a
joannayhs <joannayuhas@gmail.com>
2022-08-22 16:51:24 -0400

    added placeholder inputs for new recipe form

--- src/components/NewRecipe.js
+++ src/components/NewRecipe.js
@@ -5,7 +5,7 @@ export default function NewRecipe(){
     
     function handleSubmit(e){
         e.preventDefault()
-        console.log("Form Submitted")
+        console.log(e.value)
     }
 
 
@@ -13,6 +13,19 @@ export default function NewRecipe(){
         <>
          <form onSubmit={handleSubmit}>
             <input type="text" placeholder="Drink Name"/>
+            <input type="text" placeholder="Category"/>
+            <input type="text" placeholder="Instructions"/>
+            <input type="text" placeholder="image URL"/>
+            <input type="text" placeholder="Ingredient 1"/>
+            <input type="text" placeholder="Measurement 1"/>
+            <input type="text" placeholder="Ingredient 2"/>
+            <input type="text" placeholder="Measurement 2"/>
+            <input type="text" placeholder="Ingredient 3"/>
+            <input type="text" placeholder="Measurement 3"/>
+            <input type="text" placeholder="Ingredient 4"/>
+            <input type="text" placeholder="Measurement 4"/>
+            <input type="text" placeholder="Ingredient 5"/>
+            <input type="text" placeholder="Measurement 5"/>
             <button>Submit</button>
         </form>
         </>
