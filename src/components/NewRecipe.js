b7a58d3dd99546b8961f14b2435c105eb101ad2d
joannayhs <joannayuhas@gmail.com>
2022-08-23 15:13:54 -0400

    created dynamic ingredient fields

--- src/components/NewRecipe.js
+++ src/components/NewRecipe.js
@@ -3,6 +3,9 @@ import React, {useState} from  'react'
 
 export default function NewRecipe({addNewRecipe}){
     const [formData, setFormData] = useState([])
+    const [ingredientFields, setIngredientFields] = useState([
+      {ingredient: "Ingredient", measurement: 'Measurement'}
+    ])
     
     function handleSubmit(e){
         e.preventDefault()
@@ -13,29 +16,44 @@ export default function NewRecipe({addNewRecipe}){
         return setFormData({...formData, [e.target.name]: e.target.value}) 
     }
 
+    function mapIngredientFields(){
+       return ingredientFields.map(( field, index ) => {
+       return(
+           <>
+               <input type="text" placeholder={`${field.ingredient} ${index + 1}`} name={`strIngredient${index + 1}`} onChange={handleChange} /> <br />
+               <input type="text" placeholder={`${field.measurement} ${index + 1}`} name={`strMeasure${index + 1}`} onChange={handleChange} /> <br />
+               <button onClick={(addIngredientField)}>Add Another Ingredient</button><br/>
+           </>
+
+       ) 
+        })
+    }
+
+    function addIngredientField(){
+        setIngredientFields([...ingredientFields, {
+            ingredient: "Ingredient", measurement: "Measurement"
+        }])
+    }
 
     return (
         <>
          <form onSubmit={handleSubmit}>
+            <label>Drink Name</label><br/>
             <input type="text" placeholder="Drink Name" name="strDrink" onChange={handleChange}/> <br/>
             
+            <label>Drink Category</label><br />
             <select name="Category" placeholder="Category">
                 <option value="cocktail">Cocktail</option>
                 <option value="shot">Shot</option>
             </select><br/>
             
-            <input type="text" placeholder="Instructions" name="strInstructions"onChange={handleChange}/> <br/>
+            <label>URL for Image:</label><br />
             <input type="text" placeholder="image URL" name="strDrinkThumb"onChange={handleChange}/> <br/>
-            <input type="text" placeholder="Ingredient 1" name="strIngredient1"onChange={handleChange}/> <br/>
-            <input type="text" placeholder="Measurement 1" name="strMeasure1"onChange={handleChange}/> <br/>
-            <input type="text" placeholder="Ingredient 2" name="strIngredient2"onChange={handleChange}/> <br/>
-            <input type="text" placeholder="Measurement 2" name="strMeasure2" onChange={handleChange}/> <br/>
-            <input type="text" placeholder="Ingredient 3" name="strMeasure3" onChange={handleChange}/> <br/>
-            <input type="text" placeholder="Measurement 3" name="strMeasure3" onChange={handleChange}/> <br/>
-            <input type="text" placeholder="Ingredient 4" name="strMeasure4" onChange={handleChange}/> <br/>
-            <input type="text" placeholder="Measurement 4" name="strMeasure4" onChange={handleChange}/> <br/>
-            <input type="text" placeholder="Ingredient 5" name="strMeasure5" onChange={handleChange}/> <br/>
-            <input type="text" placeholder="Measurement 5" name="strMeasure5" onChange={handleChange}/> <br/>
+            
+            {mapIngredientFields()}
+            
+            <label>Instructions:</label><br />
+            <textarea placeholder="Instructions" name="strInstructions" onChange={handleChange} /> <br />
             <button>Submit</button>
         </form>
         </>
